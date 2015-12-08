8520a80d80dc4d97b8fdbd803f95596e5c88bca4
CodeByLine <yleventhal@ymail.com>
2015-12-08 13:41:31 +0000

    Done.

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,8 @@
+#age_of_person = current_age_for_birth_year(1984)
+def current_age_for_birth_year(birth_year)
+  2015 - birth_year
+end
+
+#def current_age_for_birth_year(birth_year)
+#  Time.now.year - birth_year
+#end
